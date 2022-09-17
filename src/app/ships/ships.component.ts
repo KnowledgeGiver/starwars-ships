83e2213600ac5a1383b6f67d8ed808eba05a1306
Angel Juarez <angeljuarez729@gmail.com>
2022-09-17 15:10:49 -0400

    injected ship service. Used service to make http request and binded info to ships variable

---
+++ src/app/ships/ships.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Ship } from '../Ship';
+import { ShipsServiceService } from '../ships-service.service';
+
+@Component({
+  selector: 'app-ships',
+  templateUrl: './ships.component.html',
+  styleUrls: ['./ships.component.css']
+})
+export class ShipsComponent implements OnInit {
+  ships: Ship[] = [];
+
+  constructor(private shipsService: ShipsServiceService) { } // dependency injection - constructor injection
+
+  ngOnInit(): void {
+    this.getShips();
+  }
+
+  getShips(): void {
+    this.shipsService.getShips()
+      .subscribe(data => this.ships = data.results);
+  }
+}
